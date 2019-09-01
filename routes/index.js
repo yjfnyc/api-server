2b2d60965f067b8b9e92719e699e1090f689141c
Jinfeng Ye <yjfnyc@gmail.com>
2019-09-01 12:31:13 -0400

    implemented returning color data

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
+const api = require('./api');
 
-/* GET home page. */
+router.use('/api', api);
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
