7f60959468fdc556d1c7f47e469aed54cae24618
Jinfeng Ye <yjfnyc@gmail.com>
2019-09-02 20:16:15 -0400

    refactor with beforeEach

--- test/unit/colors.test.js
+++ test/unit/colors.test.js
@@ -2,28 +2,27 @@ const colorsContoller = require('../../controllers/colors');
 const httpMocks = require("node-mocks-http");
 
 describe("colors controller retrieving colors ", () => {
+    let req, res;
+    beforeEach(()=>{
+        req = httpMocks.createRequest();
+        res = httpMocks.createResponse();
+    });
+
     it("should have a getColos function", () => {
         expect(typeof colorsContoller.getColors).toBe("function");
     });
 
     it("should return 200 status code", () => {
-        let req, res;
-        req = httpMocks.createRequest();
-        res = httpMocks.createResponse();
-
         colorsContoller.getColors(req, res);
         expect(res.statusCode).toBe(200);
+        expect(res._isEndCalled()).toBeTruthy();
     });
 
     it("should get json color data", () => {
-        req = httpMocks.createRequest();
-        res = httpMocks.createResponse();
         colorsContoller.getColors(req, res);
         let data = res._getJSONData();
         expect(data.colorNameHexCodes).toBeDefined();
         expect(data.colorNames).toBeDefined();
-        expect(res._isEndCalled()).toBeTruthy();
         expect(res._isJSON()).toBeTruthy();
-
     })
 });
